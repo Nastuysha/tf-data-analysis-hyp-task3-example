37594e43068e38f645006e05d619a2e2156860fd
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-13 22:58:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,7 @@ chat_id = 1141722952 # Ваш chat ID, не меняйте название пе
 def solution(x: np.array) -> bool: 
     alpha = 0.08
     p_value=ztest(x, value=500, alternative = 'larger')
-    return p_value < alpha # Ваш ответ, True или False
+    if (p_value[1]<alpha): 
+        return True 
+    else: 
+        return False
